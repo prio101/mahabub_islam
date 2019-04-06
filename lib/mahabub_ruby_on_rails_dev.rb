8276899ca73febfa7fa047d30bbf87f5eeb33dd4
Mahabub Islam <miprio101@gmail.com>
2019-04-06 09:14:02 +0600

    modification of the puts instead of heredoc

--- lib/mahabub_ruby_on_rails_dev.rb
+++ lib/mahabub_ruby_on_rails_dev.rb
@@ -7,61 +7,56 @@ module MahabubRubyOnRailsDev
   class Mahabub
 
     def self.cv
-      personal_info
-      educational_info
-      work_experiences
-      over_the_internet
+      [personal_info,
+      educational_info,
+      work_experiences,
+      over_the_internet].join
     end
 
     def self.personal_info
-      <<-HEREDOC
-        Personal Information:
-        #{'-'*100}
-        Name: Mahabub Islam
-        Email: miprio101@gmail.com
-        Personal Site: www.mahabub.space
-        Phone: +8801712-187484
-        Hobbies: Coding, Reading, Biking
-        \n
-      HEREDOC
+       puts "Personal Information:\n
+        #{'-'*100}\n
+        Name: Mahabub Islam\n
+        Email: miprio101@gmail.com\n
+        Personal Site: www.mahabub.space\n
+        Phone: +8801712-187484\n
+        Hobbies: Coding, Reading, Biking\n
+        "      
     end
     
     def self.educational_info
-      <<-HEREDOC
-        Educational Background:
-        #{'-'*100}
-        Secondary School Certificate: [ GPA 4.81 ] - B.L. Govt. High School, Sirajganj
-        Higher Secondary School Certificate: [ GPA 4.10 ] - Sirajganj Govt. College
-        BSC in CSE: Daffodil International University (Admitted)
+      puts "Educational Background:\n
+        #{'-'*100}\n
+        Secondary School Certificate: [ GPA 4.81 ] - B.L. Govt. High School, Sirajganj\n
+        Higher Secondary School Certificate: [ GPA 4.10 ] - Sirajganj Govt. College\n
+        BSC in CSE: Daffodil International University (Admitted)\n
         \n
-      HEREDOC
+        "
     end
 
     def self.work_experiences
-      <<-HEREDOC
-        Work Experiences:
-        #{'-'*100}
-        Infobase ICT : [April 2015 - March 2016], Junior Software Engineer
-            -> Created web applications with Ruby On Rails and LAMP stack.
-        Welldev.io :  [April 2016 - April 2017], Software Engineer
-            -> Created web applications with the Ruby On Rails and React JS.
-        Carriage : [April 2017 - April 2018], Software Engineer
-            -> Created web application Features with the Ruby On Rails.
-        Haltrip : [June 2018 - April 2019], Software Engineer
-            -> Creatting features for the Ruby on Rails application.
+      puts "Work Experiences:\n
+        #{'-'*100}\n
+        Infobase ICT : [April 2015 - March 2016], Junior Software Engineer\n
+            -> Created web applications with Ruby On Rails and LAMP stack.\n
+        Welldev.io :  [April 2016 - April 2017], Software Engineer\n
+            -> Created web applications with the Ruby On Rails and React JS.\n
+        Carriage : [April 2017 - April 2018], Software Engineer\n
+            -> Created web application Features with the Ruby On Rails.\n
+        Haltrip : [June 2018 - April 2019], Software Engineer\n
+            -> Creatting features for the Ruby on Rails application.\n
         \n            
-      HEREDOC
+        "
     end
 
     def self.over_the_internet
-      <<-HEREDOC
-        Over the Internet: 
-        #{'-'*100}
-        Find me here: mahabub.space
-        github: https://github.com/prio101
-        linked in: https://www.linkedin.com/in/miprio101/
-        twitter: https://twitter.com/prio_mahabub
-      HEREDOC
+      puts "Over the Internet: \n
+        #{'-'*100}\n
+        Find me here: mahabub.space\n
+        github: https://github.com/prio101\n
+        linked in: https://www.linkedin.com/in/miprio101/\n
+        twitter: https://twitter.com/prio_mahabub\n
+       "
     end
     
   end
